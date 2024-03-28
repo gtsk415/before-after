9ea4953933416a160c7866b2b552343b27501fee
gtsk415 <print.bistro@mail.ru>
2024-03-28 16:31:26 +0300

    modified css/js

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,9 @@
+"use strict"
+
 function getTempalte(state) {
   return `
     <div class="slider__before">
-      <div class="slider__resize"></div>
+      <div class="slider__resize" data-type="resize"></div>
     </div>
     <div class="slider__after"></div>
   `
@@ -9,16 +11,38 @@ function getTempalte(state) {
 
 class Slider {
   constructor (selector, state) {
-    this.$slider = documetn.getElementById(selector)
+    this.$slider = document.getElementById(selector)
     this.state = state
     this.#render(this.state)
+    this.#listen()
   }
 
   #render(state) {
     this.$slider.innerHTML = getTempalte(state)
   }
 
+  #listen() {
+    this.mouseDownHandler = this.mouseDownHendler.bind(this)
+    this.mouseUpHandler = this.mouseUpHendler.bind(this)
+    this.moveHandler = this.moveHandler.bind(this)
+    this.$slider.addEventListener('mousedown', this.mouseDownHandler)
+    this.$slider.addEventListener('mouseup', this.mouseUpHandler)
+
+  }
+  
+  mouseDownHandler(event) {
+  
+  }
+
+  mouseUpHandler(event) {
+
+  }
+
+  moveHandler(event) {
+
+  }
+
 }
 
-const slider = new Slider( 'slider', {})
+const slider = new Slider("slider", {})
 
