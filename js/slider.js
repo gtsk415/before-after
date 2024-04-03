cb0a9428cd7ddd2a05662a4d13de1e9b37d16022
gtsk415 <print.bistro@mail.ru>
2024-04-03 14:02:02 +0300

    90%

--- js/slider.js
+++ js/slider.js
@@ -2,17 +2,20 @@
 
 function getTempalte(state) {
   return `
-    <div class="slider__before">
+    <div class="slider__before" style="height: ${state.height}px; background-image: url("${state.before}")">
       <div class="slider__resize" data-type="resize"></div>
     </div>
-    <div class="slider__after"></div>
+    <div class="slider__after" style="background-image: url("${state.after}")"></div>
   `
 }
 
 class Slider {
   constructor (selector, state) {
     this.$slider = document.getElementById(selector)
-    this.state = state
+    this.state = {
+      ...state,
+      height: state.height || 512
+    }
     this.#render(this.state)
     this.#listen()
   }
@@ -21,9 +24,17 @@ class Slider {
     this.$slider.innerHTML = getTempalte(state)
   }
 
+  #update(props) {
+    this.state = {
+      ...this.state,
+      ...props
+    }
+    this.#render(this.state)
+  }
+
   #listen() {
-    this.mouseDownHandler = this.mouseDownHendler.bind(this)
-    this.mouseUpHandler = this.mouseUpHendler.bind(this)
+    this.mouseDownHandler = this.mouseDownHandler.bind(this)
+    this.mouseUpHandler = this.mouseUpHandler.bind(this)
     this.moveHandler = this.moveHandler.bind(this)
     this.$slider.addEventListener('mousedown', this.mouseDownHandler)
     this.$slider.addEventListener('mouseup', this.mouseUpHandler)
@@ -32,19 +43,25 @@ class Slider {
   
   mouseDownHandler(event) {
     if(event.target.dataset.type === 'resize') {
-      this.$slider.addEventListener
+      this.$slider.addEventListener('mousemove', this.moveHandler);
+      this.currentClientX = event.clientX;
     }
   }
 
   mouseUpHandler(event) {
-
+    this.$slider.removeEventListener('mousemove', this.moveHandler)
   }
 
   moveHandler(event) {
-
+    let newClientX = this.currentClientX - event.clientX;
+    this.#update({width: this.state.width - newClientX})
+    this.currentClientX = event.clietnX;
   }
 
 }
 
-const slider = new Slider("slider", {})
+const slider = new Slider("slider", {
+  after: './../img/after.jpg',
+  before
+})
 
